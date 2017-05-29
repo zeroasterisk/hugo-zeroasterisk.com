5c20ecadd7c87ff622792466c2f1243924011041
alan bount <alan@zeroasterisk.com>
2017-05-29 16:41:43 -0400

    made hugo publish to gh a bit more friendly

--- alan.zeroasterisk.com/publish_to_gh_pages.sh
+++ alan.zeroasterisk.com/publish_to_gh_pages.sh
@@ -11,20 +11,20 @@ then
     exit 1;
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
+# echo "Deleting old publication"
+# rm -rf public
+# mkdir public
+# git worktree prune
+# rm -rf ../.git/worktrees/public/
+#
+# echo "Checking out gh-pages branch into public"
+# git worktree add -B gh-pages public origin/gh-pages
+#
+# echo "Removing existing files"
+# rm -rf public/*
 
 echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push
