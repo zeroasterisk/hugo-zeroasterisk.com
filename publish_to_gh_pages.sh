52702f14b163de58eb04d233c6084b7935a1b8f8
alan bount <alan@zeroasterisk.com>
2017-05-29 17:03:50 -0400

    move to repo-root

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -11,17 +11,17 @@ then
     exit 1;
 fi
 
-# echo "Deleting old publication"
-# rm -rf public
-# mkdir public
-# git worktree prune
-# rm -rf ../.git/worktrees/public/
-#
-# echo "Checking out gh-pages branch into public"
-# git worktree add -B gh-pages public origin/gh-pages
-#
-# echo "Removing existing files"
-# rm -rf public/*
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
 
 echo "Generating site"
 hugo
